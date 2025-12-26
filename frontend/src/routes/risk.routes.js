b244a89ffb2e63da4d84fdcedebda13fa5851194
Mauricio Jimmy Juarez Zeballos <mauricio.juarez@unmsm.edu.pe>
2025-12-26 14:02:27 -0500

    final20

--- frontend/src/routes/risk.routes.js
+++ frontend/src/routes/risk.routes.js
@@ -1,16 +1,7 @@
-// frontend/src/routes/risk.soutes.js
-
-
 const express = require("express");
 const router = express.Router();
 const riskController = require("../controllers/risk.controller");
-
-// Forma directa (recomendada)
-module.exports = authMiddleware;
-
-// O importa así si mantienes el objeto)
-const { authMiddleware } = require("../middlewares/auth.middleware");
-
+const authMiddleware = require("../middlewares/auth.middleware");
 
 // Página de evaluación de riesgo
 router.get("/dashboard", authMiddleware, riskController.renderRisk);
